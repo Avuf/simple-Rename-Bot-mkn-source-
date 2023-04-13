437fe2234bfec47424aa1b1112b8e91269b78cbc
Avuf <127880889+Avuf@users.noreply.github.com>
2023-04-13 22:57:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
+API_ID = int(environ.get("API_ID", "29203260"))
+API_HASH = environ.get("API_HASH", "af65fe71de3f7bf8e82e2cc296c91d1b")
+BOT_TOKEN = environ.get("BOT_TOKEN", "5998540233:AAGbnJREDaxmSAnB49Iiwgd6ZQpATO2YeFg")
+ADMIN = int(environ.get("ADMIN", "5305162074"))          
+CAPTION = environ.get("CAPTION", "@pfpandwallpaper")
 
 class temp(object):
     THUMBNAIL = environ.get("THUMBNAIL", "")
